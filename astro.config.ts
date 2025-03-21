42178f76767d1468902447b9909b3f2adb54da22
Ashley Harrison <ashley.harrison@grafana.com>
2025-03-21 13:47:28 +0000

    add github pages action/config

--- astro.config.ts
+++ astro.config.ts
@@ -6,5 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  // github pages config
+  site: 'https://ashanna.wedding',
 });

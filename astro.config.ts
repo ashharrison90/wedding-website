bfbaf616182415deb094b3a286f23c7d54d16a35
Anna Thomas <anna.thomas@hudl.com>
2025-03-22 13:15:45 +0000

    Move disco ball to react component

--- astro.config.ts
+++ astro.config.ts
@@ -2,12 +2,17 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   // github pages config
   site: "https://ashanna.wedding",
+
   trailingSlash: "never",
+  integrations: [react()],
 });

318141b61a005bf5df2680d4a3d1857adde07412
Anna Thomas <anna.thomas@hudl.com>
2025-03-22 15:31:43 +0000

    Remove node adapter

--- astro.config.ts
+++ astro.config.ts
@@ -2,12 +2,8 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-
   vite: {
     plugins: [tailwindcss()],
   },
@@ -16,8 +12,4 @@ export default defineConfig({
   site: "https://ashanna.wedding",
 
   trailingSlash: "never",
-
-  adapter: node({
-    mode: "standalone",
-  }),
 });

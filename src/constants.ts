71d7cb794f995f302d8d1a92e19b85b9a0b482b1
Ashley Harrison <ashley.harrison@grafana.com>
2025-03-22 11:12:02 +0000

    fix date formatting

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,6 @@
 export const WEDDING_DATE = new Date("2026-07-18T00:00:00");
 export const NAME_1 = "Ash";
 export const NAME_2 = "Anna";
+// a valid BCP 47 language tag
+// see https://gist.github.com/typpo/b2b828a35e683b9bf8db91b5404f1bd1
+export const DATE_FORMAT: Intl.LocalesArgument = "en-GB";
